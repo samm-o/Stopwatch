bf96ffa907ee274032281ae9dce49a69ba2b6da4
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:28:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var stopwatch = new Stopwatch();
+                Console.Write("Press enter to start: ");
+                stopwatch.Start();
+
+                Console.Write("Press enter to stop: ");
+                stopwatch.Stop();
+
+                Console.WriteLine("Your duration is " + stopwatch.Caclulate());
+            }
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime startTime { get; set; }
+        public DateTime stopTime { get; set; }
+
+        public void Start()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input) == true)
+                {
+                    startTime = DateTime.Now;
+                }
+                else
+                {
+                    Console.Write("Invalid input please press enter to start: ");
+                    i--;
+                    continue;
+                }
+            }
+            Console.Clear();
+        }
+        public void Stop()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input) == true)
+                {
+                    stopTime = DateTime.Now;
+                }
+                else
+                {
+                    Console.Write("Invalid input please press enter to stop: ");
+                    i--;
+                    continue;
+                }
+            }
+            Console.Clear();
+        }
+        public TimeSpan Caclulate()
+        {
+            return stopTime - startTime;
+        }
+    }
+}
